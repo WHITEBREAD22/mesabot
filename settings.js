abec414bfb57e2dfeedb6c53d16ab025b50cc389
WHITEBREAD22 <109132584+WHITEBREAD22@users.noreply.github.com>
2022-07-13 06:29:54 +0300

    Updatesettings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['+447441450082'] //ur owner number
+global.ownername = "‌🔱🇳‌🇪‌🇦‌🇷🔱" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
+global.owner = ['+447441450082']
+global.ownertag = '+447441450082' //wa.me/+447441450082
+global.botname = '🔥L-LAWLIET🔥' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.packname = "☘️L-LAWLIET☘️" //ur sticker watermark packname
+global.author = "🤞🏻RYUZUKI-L🤞🏻" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['+447441450082'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -106,19 +106,19 @@ global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
-    admin: 'This Feature Is Only For Admin!',
-    botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!',
-    group: 'Feature Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
-    error: 'Error!!',
+    admin: '♕☘️آلآمـر للمـشـرفين فقطـ يسـطـآ☘️♕!',
+    botAdmin: '🤞🏻لآزمـ آكون آدمـن يسـطـآ🤞🏻!',
+    owner: '🔱آلآمـر للح ـآكمـ آلآعظـمـ يسـطـآ🔱!',
+    group: '🙂آلآمـر للكروبــآت فقطـ يسـطـآ🙂!',
+    private: '🥀آلآمـر للخ ـآصـ فقطـ يسـطـآ🥀!',
+    bot: '🌺آلآمـر للبــوت فقطـ يسـطـآ🌺',
+    wait: '✍🏻يرج ـى آلآنتظـآر مـن فضـلك✍🏻...',
+    linkm: 'وين آلرآبــطـ يسـطـآ🗿?',
+    error: 'وشـ سـويت يسـطـآ😢!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
-    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    ban: 'بلعت بلوك من البوت. اتكلم مع المعلم ليرفع عنك البلوك',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
-    banChat: 'The bot was banned in this group, please contact the owner to unban'
+    banChat: 'البوت انحظر فهذا الكروب. يرجى التكلم مع صاحبه لفك البلوك'
 }
     global.limitawal = {
     premium: "Infinity",
